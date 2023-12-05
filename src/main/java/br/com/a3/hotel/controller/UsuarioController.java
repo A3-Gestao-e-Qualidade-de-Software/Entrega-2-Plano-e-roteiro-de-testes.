b5c024bd1b384ca1f51b55e642d2b5b906497422
Daniel Cortez <106662629+Corttezz@users.noreply.github.com>
2023-12-04 21:22:57 -0300

    Update UsuarioController.java

--- src/main/java/br/com/a3/hotel/controller/UsuarioController.java
+++ src/main/java/br/com/a3/hotel/controller/UsuarioController.java
@@ -9,23 +9,35 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UsuarioController {
+
+    // Método para realizar o login do usuário
     public int fazerLogin() throws SQLException, ClassNotFoundException {
+        // Cria uma instância da view para autenticação do usuário
         UsuarioView usuario_view = new UsuarioView();
+        // Recebe os dados de login inseridos pelo usuário
         UsuarioModel Usuario = usuario_view.autenticarUsuario();
+        // Cria uma instância do DAO para lidar com a autenticação
         UsuarioDAO usuario_DAO = new UsuarioDAO();
+        // Realiza a consulta no banco de dados para autenticar o usuário
         ResultSet resultSet = usuario_DAO.autenticarUsuario(Usuario);
-        if (resultSet.next()){
+
+        // Verifica se a consulta retornou algum resultado válido
+        if (resultSet.next()) {
+            // Exibe uma mensagem de login bem-sucedido
             JOptionPane.showMessageDialog(null, "Login feito com sucesso!!");
+            // Exibe o menu para o usuário logado e aguarda uma resposta válida
             int menu = View.AppView.menuLogado();
-            while (menu < 1 || menu > 4){
+            while (menu < 1 || menu > 4) {
                 menu = View.AppView.menuLogado();
             }
-            return menu;
-        }else {
+            return menu; // Retorna o menu escolhido pelo usuário
+        } else {
+            // Caso a autenticação falhe, exibe uma mensagem e redireciona para o início do aplicativo
             JOptionPane.showMessageDialog(null, "Funcional e/ou Senha Inválida!");
             AppController ac = new AppController();
-            ac.iniciar();
-            return 4;
+            ac.iniciar(); // Redireciona para o início do aplicativo
+            return 4; // Retorna a opção de encerrar o sistema
         }
     }
 }
+
