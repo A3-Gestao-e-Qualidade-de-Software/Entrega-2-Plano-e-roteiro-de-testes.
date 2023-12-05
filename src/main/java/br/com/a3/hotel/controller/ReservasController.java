2fe61fd6faacf18e6985684d56635b36a2674b2a
Daniel Cortez <106662629+Corttezz@users.noreply.github.com>
2023-12-04 21:22:13 -0300

    Update ReservasController.java

--- src/main/java/br/com/a3/hotel/controller/ReservasController.java
+++ src/main/java/br/com/a3/hotel/controller/ReservasController.java
@@ -7,7 +7,9 @@ import java.sql.SQLException;
 public class ReservasController {
 
     public static void cadastrarReserva() throws SQLException, ClassNotFoundException {
+        // Cria uma nova instância da view para cadastro de reservas
         ReservasView reservasView = new ReservasView();
+        // Chama o método na view para cadastrar uma nova reserva
         ReservasView.cadastrarNovaReserva();
     }
 }
