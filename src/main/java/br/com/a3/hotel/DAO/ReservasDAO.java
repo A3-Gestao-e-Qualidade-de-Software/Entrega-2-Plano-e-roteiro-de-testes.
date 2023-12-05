69a0cd957a60ced7a3ade828df2120529c5b07bf
Daniel Cortez <106662629+Corttezz@users.noreply.github.com>
2023-12-04 21:17:26 -0300

    Update ReservasDAO.java

--- src/main/java/br/com/a3/hotel/DAO/ReservasDAO.java
+++ src/main/java/br/com/a3/hotel/DAO/ReservasDAO.java
@@ -5,6 +5,16 @@ import br.com.a3.hotel.model.ReservasModel;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+    /**
+     * Cadastra uma nova reserva no banco de dados.
+     *
+     * @param objReservasModel O objeto ReservasModel contendo as informações da reserva a ser cadastrada.
+     * @return true se o cadastro for bem-sucedido, false caso contrário.
+     * @throws SQLException             Se ocorrer um erro durante a execução da operação no banco de dados.
+     * @throws ClassNotFoundException Se o driver JDBC não puder ser carregado.
+     */
+
+
 public class ReservasDAO {
 
 //    public boolean cadastrarReserva(ReservasModel objReservasModel) throws SQLException, ClassNotFoundException {
